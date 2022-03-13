2d49777dc186e88c21e809c843d03cbddf5998b3
Ankit <radheyshyam91647127@gmail.com>
2022-03-13 14:58:24 +0530

    updatee

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ app.use(express.urlencoded());
 app.set('view engine','ejs');
 app.set('views', './views');
 
-app.use(express.static('assets'));
 
 app.use('/',function(req,res){
     return res.render('home');
 });
 
-const db= require('./mongoose');
+// const db= require('./mongoose');
 
 
 app.listen(port,function(req,res){
